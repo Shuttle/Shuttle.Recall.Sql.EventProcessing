05e35b770ddab56e1360892064c92baf4dd198df
Eben <me@ebenroux.co.za>
2017-07-02 11:47:46 +0200

    - updated tests

---
+++ Shuttle.Recall.Sql.EventProcessing.Tests/StorageFixture.cs
@@ -0,0 +1,25 @@
+﻿using Castle.Windsor;
+using NUnit.Framework;
+using Shuttle.Core.Castle;
+using Shuttle.Core.Data;
+using Shuttle.Core.Infrastructure;
+using Shuttle.Recall.Tests;
+
+namespace Shuttle.Recall.Sql.EventProcessing.Tests
+{
+    public class StorageFixture : Fixture
+    {
+        [Test]
+        public void ExerciseEventProcessing()
+        {
+            var container = new WindsorComponentContainer(new WindsorContainer());
+
+            EventStore.Register(container);
+
+            using (container.Resolve<IDatabaseContextFactory>().Create(EventStoreConnectionStringName))
+            {
+                RecallFixture.ExerciseEventProcessing(EventStore.Create(container), EventProcessor.Create(container));
+            }
+        }
+    }
+}
