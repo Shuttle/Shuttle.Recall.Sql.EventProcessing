e01b1c7fa1ca9dd4068531fb056eb8694e95adff
Eben <me@ebenroux.co.za>
2020-01-24 16:11:43 +0200

    - nuspec

---
+++ Shuttle.Recall.Sql.EventProcessing.Tests/EventProcessingFixture.cs
@@ -0,0 +1,27 @@
+﻿using Castle.Windsor;
+using NUnit.Framework;
+using Shuttle.Core.Castle;
+using Shuttle.Core.Data;
+using Shuttle.Recall.Tests;
+using Shuttle.Core.Container;
+
+namespace Shuttle.Recall.Sql.EventProcessing.Tests
+{
+    public class EventProcessingFixture : Fixture
+    {
+        [Test]
+        public void ExerciseEventProcessing()
+        {
+            var container = new WindsorComponentContainer(new WindsorContainer());
+
+            Boostrap(container);
+
+            EventStore.Register(container);
+
+            using (container.Resolve<IDatabaseContextFactory>().Create(EventStoreConnectionStringName))
+            {
+                RecallFixture.ExerciseEventProcessing(EventStore.Create(container), EventProcessor.Create(container), 300);
+            }
+        }
+    }
+}
