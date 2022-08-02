61c27cbc06774b6af739e9b13f73ccf28ae09914
Eben <me@ebenroux.co.za>
2022-08-02 07:15:27 +0200

    Update EventProcessingOptions.cs

--- Shuttle.Recall.Sql.EventProcessing/EventProcessingOptions.cs
+++ Shuttle.Recall.Sql.EventProcessing/EventProcessingOptions.cs
@@ -4,6 +4,8 @@ namespace Shuttle.Recall.Sql.EventProcessing
 {
     public class EventProcessingOptions
     {
+        public const string SectionName = "Shuttle:EventProcessing";
+
         public string EventStoreConnectionStringName { get; set; }
         public string EventProjectionConnectionStringName { get; set; }
     }
